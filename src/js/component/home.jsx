ef2ca7f143150f535012833e0f2d19df862f16b9
Me21cr <128864515+Me21cr@users.noreply.github.com>
2023-08-01 04:53:17 +0000

    cambios

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,25 +1,59 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
+	const [tarea, setTarea] = useState("")
+	const [lista, setLista] = useState(["lavar el carro", "ir al cine"])
+
+	const handleInput = (e) => {
+		let texto = e.target.value
+		if (e.keyCode == 13) {
+			setTarea(texto)
+			//Una primera aproximación para agregar a la lista es usando una variable auxiliar
+			//let tempArr = lista.slice() //copia de arreglo por valor
+			//tempArr.push(texto)
+			//setLista(tempArr)
+
+			//Una segunda aproximación es usando el operador spread ...
+			setLista([...lista, texto])
+		}
+	}
+
+	const deleteTask = (index) => {
+		let tempArr = lista.slice() //copiar el estado lista en una variable auxiliar
+		tempArr = tempArr.filter((item, index2) => { return index2 != index })
+		setLista(tempArr)
+	}
+
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
+		<>  <h1>Todos</h1>
+			<div class="paper">
+					<ul>
+						<input className="texto" placeholder="What needs to be done"
+								onKeyUp={
+									(e) => { handleInput(e) }
+								} />
+						{
+							lista && lista.length > 0 ?
+								<>{
+									lista.map((item, index) => {
+										return <li key={index}>
+
+											{item}
+											<button className="boton" type="button" onClick={e => { deleteTask(index) }}>
+											X
+											</button>
+										</li>
+									})
+								}</>
+								: "0 Task"
+						}
+					</ul>
+				</div >
+		</>
 	);
 };
 
